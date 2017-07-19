464421d430d14304354b4e7a06542f135f288b3f
ledangtuanbk <ledangtuanbk@users.noreply.github.com>
2017-07-19 09:46:36 +0700

    init project

---
+++ app/src/main/java/com/ldt/library/MyHelper.java
@@ -0,0 +1,15 @@
+package com.ldt.library;
+
+import android.util.Log;
+
+/**
+ * Created by ldt on 7/19/2017.
+ */
+
+public class MyHelper {
+    private static final String TAG = MyHelper.class.getSimpleName();
+
+    public static void print(){
+        Log.d(TAG, "print() called");
+    }
+}
